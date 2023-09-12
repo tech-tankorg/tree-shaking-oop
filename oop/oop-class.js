fe8e67e3da43c733f7279dd343e2d735108dad71
Nonso <nonsootoh@gmail.com>
2023-09-12 13:30:12 -0400

    init

---
+++ oop/oop-class.js
@@ -0,0 +1,4 @@
+// Construct the following class to implement the methods described in the README
+class SlackApp {}
+
+module.exports = { App: SlackApp };
---
+++ tree-shaking-complete/tree-shake.js
@@ -0,0 +1,3 @@
+// Construct the following functions to implement the methods described in the README
+
+// HINT: You first need to create a function that initialize an app
